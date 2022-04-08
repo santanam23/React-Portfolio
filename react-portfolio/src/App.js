c26b91111a5c596b54763bf9a725eacf7cd11e20
santanam23 <mirmir11.ms@gmail.com>
2022-04-08 17:30:48 -0500

    created tests

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import About from './components/About'
+import ContactForm from './components/Contact';
 
 function App() {
   return (
     <div>
       <main>
         <About></About>
+        <ContactForm></ContactForm>
       </main>
     </div>
   );
--- react-portfolio/src/components/Nav/index.js
+++ react-portfolio/src/components/Nav/index.js
@@ -4,8 +4,24 @@ function Nav() {
 
   return (
     <header>
+    <h2>
+      <a href="/">
+        <span role="img" aria-label="camera"> 📸</span> Miriams Portfolio
+      </a>
+    </h2>
+    <nav>
+      <ul className="flex-row">
+        <li className="mx-2">
+          <a href="#about">
+            About me
+          </a>
+        </li>
+        <li>
+          <span>Contact</span>
+        </li>
+      </ul>
+    </nav>
     </header>
   );
 }
-
 export default Nav;
