a544c88beabaf29a1f924f6887f74f0612d62607
santanam23 <mirmir11.ms@gmail.com>
2022-04-11 10:38:36 -0500

    adding who i am info

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -7,8 +7,8 @@ function About() {
       <h1 id="about">Who am I?</h1>
       <div className="my-2">
         <p>
-          This will be the about me section.
-      </p>
+        I graduated from the University of Houston with a BA in Communications in 2015. After college, I decided to stay in my line of work and become a General Manager for Chipotle Mexican Grill. I was a General Manager for six years. During the pandemic, I realized I wanted to go back to school and focus on my passion for tech. In May 2020, I placed my resignation and decided to enroll in a coding boot camp. Rice University had a program starting in November so I decided to wait until Novenmber to start school. 
+        </p>
       </div>
       {/* <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" /> */}
     </section>
