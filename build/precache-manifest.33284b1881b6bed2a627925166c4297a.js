51e640a95a66011e33dbc641ed0bd5a318407915
santanam23 <mirmir11.ms@gmail.com>
2022-04-17 19:22:00 -0500

    fixing errors

---
+++ build/precache-manifest.33284b1881b6bed2a627925166c4297a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2b9a8eba52656dd6432",
+    "url": "/React-Portfolio/static/css/main.49b3ba3e.chunk.css"
+  },
+  {
+    "revision": "b2b9a8eba52656dd6432",
+    "url": "/React-Portfolio/static/js/main.b2b9a8eb.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/React-Portfolio/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "5adda2a6fdd4a6a91eac",
+    "url": "/React-Portfolio/static/js/2.5adda2a6.chunk.js"
+  },
+  {
+    "revision": "c9b177dba76876f7a19a",
+    "url": "/React-Portfolio/static/js/runtime~main.c9b177db.js"
+  },
+  {
+    "revision": "37b4ff750206f68e43b869b5c029a21f",
+    "url": "/React-Portfolio/static/media/foodFest.37b4ff75.png"
+  },
+  {
+    "revision": "dd64101600e1ea145e15366a10440114",
+    "url": "/React-Portfolio/static/media/notetaker.dd641016.png"
+  },
+  {
+    "revision": "43f61cc8e69eb7aa22b8af4ba124ea71",
+    "url": "/React-Portfolio/static/media/passwordGen.43f61cc8.png"
+  },
+  {
+    "revision": "c52a2195ee9240567f529b9c2297f275",
+    "url": "/React-Portfolio/static/media/weatherDash.c52a2195.png"
+  },
+  {
+    "revision": "21756e2f9a4e6942a8362e43950cbfb7",
+    "url": "/React-Portfolio/index.html"
+  }
+];
