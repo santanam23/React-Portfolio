1b6526640a632715c46336cdd21792a66c39e6a2
santanam23 <mirmir11.ms@gmail.com>
2022-05-12 20:37:22 -0500

    editing who i am section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import coverImage from '../../assets/images/coverImage.jpeg';
 function About() {
   return (
     <div className="my-5" id="about">
@@ -7,7 +7,7 @@ function About() {
         <p>
         I graduated from the University of Houston with a BA in Communications in 2015. After college, I decided to stay in my line of work and become a General Manager for Chipotle Mexican Grill. I was a General Manager for six years. During the pandemic, I realized I wanted to go back to school and focus on my passion for tech. In May 2020, I placed my resignation and decided to enroll in a coding boot camp. Rice University had a program starting in November so I decided to wait until Novenmber to start school. 
         </p>
-      {/* <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" /> */}
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
     </div>
   );
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
     return (
         <div className="home">
-        <h1>Welcome To My Home Page</h1>
+        <h1>Welcome To Miriam's Portfolio!</h1>
         <p>
          I am so excited you have come across my homepage. I have created this react style website for you to read a little about me. Although the react app site needs a little bit more design, I hope you like what you see so far. Feel free to reach out to me if you have any questions or corners.
         </p>
