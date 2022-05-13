d5f0d288351236dda8c2b845cf0523f494d6b4c5
santanam23 <mirmir11.ms@gmail.com>
2022-05-13 10:22:14 -0500

    Updates

---
+++ precache-manifest.3f5f2adaf97537bb1a92e0e236e5dec8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "70d604f0e502af3692e0",
+    "url": "/React-Portfolio/static/css/main.6a862f84.chunk.css"
+  },
+  {
+    "revision": "70d604f0e502af3692e0",
+    "url": "/React-Portfolio/static/js/main.70d604f0.chunk.js"
+  },
+  {
+    "revision": "60e457aa0d153a0c866c",
+    "url": "/React-Portfolio/static/js/1.60e457aa.chunk.js"
+  },
+  {
+    "revision": "d849e8225f8ea7d378c5",
+    "url": "/React-Portfolio/static/js/2.d849e822.chunk.js"
+  },
+  {
+    "revision": "5b20d7d7d15c0fdde0ff",
+    "url": "/React-Portfolio/static/js/runtime~main.5b20d7d7.js"
+  },
+  {
+    "revision": "37b4ff750206f68e43b869b5c029a21f",
+    "url": "/React-Portfolio/static/media/foodFest.37b4ff75.png"
+  },
+  {
+    "revision": "dd64101600e1ea145e15366a10440114",
+    "url": "/React-Portfolio/static/media/notetaker.dd641016.png"
+  },
+  {
+    "revision": "43f61cc8e69eb7aa22b8af4ba124ea71",
+    "url": "/React-Portfolio/static/media/passwordGen.43f61cc8.png"
+  },
+  {
+    "revision": "c52a2195ee9240567f529b9c2297f275",
+    "url": "/React-Portfolio/static/media/weatherDash.c52a2195.png"
+  },
+  {
+    "revision": "5ca348bc6f8078a4c3ad8a453afc4b1c",
+    "url": "/React-Portfolio/static/media/houstonVolleyballSocial.5ca348bc.png"
+  },
+  {
+    "revision": "39afebdeef5d9f6ca75afc03e76864a0",
+    "url": "/React-Portfolio/index.html"
+  }
+];
